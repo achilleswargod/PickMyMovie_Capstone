c1d8c882d632cfb25622e54028366b21f5d5b7e0
achilleswargod <65618715+achilleswargod@users.noreply.github.com>
2022-05-04 16:13:47 -0500

    added the movielibrary app to backend settings + made migrations for the movie library model + created first GET endpoint in movielibrary Views.py

--- backend/movielibrary/views.py
+++ backend/movielibrary/views.py
@@ -1,3 +1,14 @@
+from urllib import response
 from django.shortcuts import render
+from backend.movielibrary import serializers
+
+from backend.movielibrary.models import Movie
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+class MovieList(APIView):
+    def get(self, request);
+        movie = Movie.objects.all()
+        serializer = serializers.MovieSerializer(movie, many=True)
+        return Response(serializer.data)
