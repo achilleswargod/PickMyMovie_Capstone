34526745a07851058bcbd7e8e14b56e752a172ba
achilleswargod <65618715+achilleswargod@users.noreply.github.com>
2022-05-26 11:29:37 -0500

    just fixing a bug in vs code

--- frontend/src/components/DisplayList/DisplayMovieList.jsx
+++ frontend/src/components/DisplayList/DisplayMovieList.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 
 const DisplayMovieList = (props) => {
-  const Image_path = "https://image.tmdb.org/t/p/w500"
+  const Image_path = "https://image.tmdb.org/t/p/w500";
   return (
     <>
-      {props.movies.map((movie, index) =>(
-        <div key={movie.id}>
-          {movie.poster_path ? <img className={'movie-cover'} src={`${Image_path}${movie.poster_path}`} alt='movie'></img> : null}
+      {props.movies.map((movie, { selectMovie }) => (
+        <div key={movie.id} onClick={() => selectMovie(movie)}>
+          {movie.poster_path ? (
+            <img
+              className={"movie-cover"}
+              src={`${Image_path}${movie.poster_path}`}
+              alt="movie"
+            ></img>
+          ) : (
+            <div className={"movie-placeholder"}> No Image Found</div>
+          )}
           <h4 className={"movie-title"}>{movie.original_title}</h4>
           <div className="card-content">
-            <p><a href="Movie Details">View Details</a></p>
+            <p>
+              <a href="Movie Details">View Details</a>
+            </p>
           </div>
         </div>
       ))}
-      
     </>
   );
-      }
+};
 
 export default DisplayMovieList;
--- frontend/src/components/DisplayList/MovieListPage.jsx
+++ frontend/src/components/DisplayList/MovieListPage.jsx
@@ -1,58 +1,64 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import DisplayMovieList from "./DisplayMovieList";
-import MovieListHeader from "./MovieHeader"
-import './MovieListPage.css';
+import MovieListHeader from "./MovieHeader";
+import "./MovieListPage.css";
 import SearchBar from "../SearchBar";
-
+import Youtube from "react-youtube";
 
 function MovieListPage() {
   const [movies, setMovies] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const [selectedMovie, setSelectedMovie] = useState({});
-  const Image_path = "https://image.tmdb.org/t/p/w1280"
+  const Image_path = "https://image.tmdb.org/t/p/w1280";
 
   const getMovieRequest = async (searchTerm) => {
-		try {
-      let response = await axios.get( `https://api.themoviedb.org/3/search/movie?api_key=0a2120a41a7a10a89778afb18eb3fd0b&query=${searchTerm}`);
+    try {
+      let response = await axios.get(
+        `https://api.themoviedb.org/3/search/movie?api_key=0a2120a41a7a10a89778afb18eb3fd0b&query=${searchTerm}`
+      );
       console.log("Movie Search: ", response.data.results);
 
       //did this so that the info would never return "undefined"
-      if(response.data.results)
-      {
+      if (response.data.results) {
         setMovies(response.data.results);
-        setSelectedMovie(response.data.results[0])
+        setSelectedMovie(response.data.results[0]);
       }
-		} catch(error){
+    } catch (error) {
       console.log(error);
     }
-	};
+  };
 
   useEffect(() => {
-    getMovieRequest(searchTerm)
+    getMovieRequest(searchTerm);
   }, [searchTerm]);
 
-  
-  
   return (
-    <div className='container movie-app'>
-      <div className='movie-search-bar'>
-        <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+    <div className="container movie-app">
+      <div className="movie-search-bar">
+        <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
       {/*set the backdrop to be an object at the top of the page*/}
-      <div className="hero"  style={{backgroundImage: `url('${Image_path}${selectedMovie.backdrop_path}')`}}>
-        <div className="hero-content">
-          <button className="hero-button">Play Trailer</button>
-          <h1>{selectedMovie.title}</h1>
+      <div
+        className="hero"
+        style={{
+          backgroundImage: `url('${Image_path}${selectedMovie.backdrop_path}')`,
+        }}
+      >
+        <div className="hero-content max-center">
+          <Youtube />
+          <button className="button">Play Trailer</button>
+          <h1 className="hero-movie-title">{selectedMovie.title}</h1>
           {/* If the movie does not have an overview it will not show*/}
-         {selectedMovie.overview ?  <p className="the-overview">{selectedMovie.overview}</p>: null}
+          {selectedMovie.overview ? (
+            <p className="the-overview">{selectedMovie.overview}</p>
+          ) : null}
         </div>
       </div>
-        <MovieListHeader heading='Movies' />
-        <div className='row'>
-          <DisplayMovieList movies={movies} />
-        </div>
-          
+      <MovieListHeader heading="Movies" />
+      <div className="row">
+        <DisplayMovieList movies={movies} />
+      </div>
     </div>
   );
 }
